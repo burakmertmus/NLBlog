d8766cda5ed0ab2dd2de03540826457e958cabec
Burak Mert Muş <burakmertmus@gmail.com>
2021-09-12 13:49:45 +0300

    Result init

---
+++ NLBLog.Shared/Utilities/Results/Abstract/IDataResult.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLBLog.Shared.Utilities.Results.Abstract
+{
+    public interface IDataResult<out T>:IResult
+    {
+        public T Data { get;} //new DataResult<Category>(ResultSatatus.Success,category)
+                              //new DataResult<IList<Category>>(ResultSatatus.Success,category
+    }
+}
